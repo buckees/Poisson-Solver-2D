fa15052a223d7a21989c8c51d606454f674bbf7a
buckees <67809187+buckees@users.noreply.github.com>
2020-09-01 10:39:13 -0700

    Add Curl Calc to get B field

--- poisson_solver_2d_example2.py
+++ poisson_solver_2d_example2.py
@@ -45,25 +45,48 @@ print(A)
 # update RHS:
 #b[4], b[9], b[14] = -100, -100, -100
 #b[int(mn/2)] = -MU0*I/(2.0*pi)
-b[0] = -MU0*I/(2.0*pi)
+b[0] = MU0*I/(2.0*pi)
 b[-1] = MU0*I/(2.0*pi)
  
 phi=scipy.linalg.solve(A,b)
 phi = phi.reshape((nx-2, ny-2))
 phi = np.pad(phi, pad_width=1, mode='constant', constant_values=0)
 
+def curl(A):
+    """
+    calculate the curl of A, which is a matrix of Fz in z direction
+    Boundary of A is set to zero
+    Using center difference, only A[1:-2, 1:-2] is calculated
+    """
+    U = np.zeros_like(A)
+    V = np.zeros_like(A)
+    bf = np.zeros_like(A)
+    nx, ny = A.shape
+    for i in range(1, nx-2):
+        for j in range(1, ny-2):
+            U[i,j] = (A[i, j+1] - A[i, j-1])/2.0
+            V[i,j] = -(A[i+1, j] - A[i-1, j])/2.0
+    bf = np.sqrt(np.power(U, 2) + np.power(V, 2))
+    U = np.divide(U, bf, where=bf>0, out=np.zeros_like(bf))
+    V = np.divide(V, bf, where=bf>0, out=np.zeros_like(bf))
+    return U, V, bf
+
+U, V, bf = curl(phi)
+
 phi = abs(phi)
 print('B field min = %.2e max = %.2e' % (phi.min(), phi.max()))
 
 #
-fig, ax = plt.subplots(figsize=(4,4))
-ax.plot(mesh.posx, mesh.posy, '.k')
+fig, ax = plt.subplots(1, 2, figsize=(6,3))
+ax[0].plot(mesh.posx, mesh.posy,
+           marker='.', markersize=3,
+           color='black', linestyle='None')
 # Alternatively, you can manually set the levels
 # and the norm:
 lev_exp = np.arange(np.floor(np.log10(phi[np.nonzero(phi)].min())),
                     np.ceil(np.log10(phi.max())), 0.1)
 levs = np.power(10, lev_exp)
-cs = ax.contour(mesh.posx, mesh.posy, phi, levs, norm=colors.LogNorm())
+cs = ax[0].contour(mesh.posx, mesh.posy, phi, levs, norm=colors.LogNorm())
 #ax.clabel(cs, cs.levels)
 #fig.colorbar(cs)
 #ax.quiver(mesh.posx, mesh.posy, vx, vy)
@@ -71,3 +94,13 @@ cs = ax.contour(mesh.posx, mesh.posy, phi, levs, norm=colors.LogNorm())
 #        color='red', marker='o', markersize=15)
 #ax.plot(pos2[0], pos2[1],
 #        color='red', marker='o', markersize=15)
+
+ax[1].plot(mesh.posx, mesh.posy,
+           marker='.', markersize=3,
+           color='black', linestyle='None')
+
+lev_exp = np.arange(np.floor(np.log10(bf[np.nonzero(bf)].min())),
+                    np.ceil(np.log10(bf.max())), 0.1)
+levs = np.power(10, lev_exp)
+cs = ax[1].contour(mesh.posx, mesh.posy, bf, levs, norm=colors.LogNorm())
+
